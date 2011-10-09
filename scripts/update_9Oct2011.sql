46b71f4d00443810bd04b7fa16a4f9380a46f5a0
Aquagen <s.lezhnev@gmail.com>
2011-10-09 19:14:33 +0400

    Добавлен скрипт обновления

---
+++ scripts/update_9Oct2011.sql
@@ -0,0 +1,18 @@
+﻿DROP TABLE financialoperationchanges;
+CREATE TABLE financialoperationchanges
+(
+  changeid integer NOT NULL,
+  foid integer,
+  changedate date,
+  whatchanged text,
+  whomchanged character varying(255),
+  CONSTRAINT financialoperationchanges_pkey PRIMARY KEY (changeid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE financialoperationchanges OWNER TO director;
+GRANT ALL ON TABLE financialoperationchanges TO director;
+GRANT ALL ON TABLE financialoperationchanges TO "armDirectors";
+GRANT ALL ON TABLE financialoperationchanges TO "armUsers";
+GRANT SELECT, REFERENCES, TRIGGER ON TABLE financialoperationchanges TO "armViewers";
